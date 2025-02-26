f681c7f77a89fdbf7f6a31df71467eda4b64c46d
David Nguyen Blot <d.nguyenblot@gmail.com>
2025-02-26 14:53:23 +0100

    replace babel in dev dependencies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,22 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 
 const Home = () => {
-    return (
-        <div className='home'>
-            <Navigation />
-            <div className="homeContent">
-                <div className="content">
-                    <h1>David Blot</h1>
-                    <h2>Développeur Web</h2>
-                    <div className="pdf">
-                        <a href="./media/DevWeb.pdf" target='_blank'>Télécharger mon CV</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="home">
+			<Navigation />
+			<div className="homeContent">
+				<div className="content">
+					<h1>David Nguyen Blot</h1>
+					<h2>Développeur Web React | Node</h2>
+					<div className="pdf">
+						<a href="./media/DevWeb.pdf" target="_blank">
+							Télécharger mon CV
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
